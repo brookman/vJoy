ad7e51d92d16a55f8adbf84ffe60d17f0d77a050
Beni Bryce Brookman <rc-p90@bluemail.ch>
2013-02-05 01:42:03 +0100

    Cleanup

--- vJoy/src/eu32k/vJoy/VJoyMain.java
+++ vJoy/src/eu32k/vJoy/VJoyMain.java
@@ -103,8 +103,12 @@ public class VJoyMain extends App2D {
 
    @Override
    public void draw(float delta) {
-      Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1);
-      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+      Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+      Gdx.gl.glEnable(GL20.GL_BLEND);
+      Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
       screenStage.draw();
 
       boolean drawHud = !Gdx.input.isKeyPressed(Input.Keys.SPACE) && Gdx.graphics.getHeight() - Gdx.input.getY() > 10;
