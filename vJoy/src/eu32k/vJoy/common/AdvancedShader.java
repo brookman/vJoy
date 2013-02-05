8af54f30fbb7b11bded4f9344bacca0dfe290a70
Beni Bryce Brookman <rc-p90@bluemail.ch>
2013-02-05 01:41:36 +0100

    Added clear instructions in FrameBuffer rendering.

--- vJoy/src/eu32k/vJoy/common/AdvancedShader.java
+++ vJoy/src/eu32k/vJoy/common/AdvancedShader.java
@@ -1,5 +1,6 @@
 package eu32k.vJoy.common;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
@@ -32,6 +33,8 @@ public class AdvancedShader extends ShaderProgram {
 
       if (frameBuffer != null) {
          frameBuffer.begin();
+         Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
       }
       if (flip) {
          PrimitivesFactory.QUAD_FLIPPED.render(this, GL20.GL_TRIANGLE_FAN);
