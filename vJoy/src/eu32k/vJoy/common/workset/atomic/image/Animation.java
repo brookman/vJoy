ee8c90613380862ce8025fab9573009209630040
brookman <rc-p90@bluemail.ch>
2013-08-06 15:21:39 +0200

    Experiments

--- vJoy/src/eu32k/vJoy/common/workset/atomic/image/Animation.java
+++ vJoy/src/eu32k/vJoy/common/workset/atomic/image/Animation.java
@@ -1,6 +1,12 @@
 package eu32k.vJoy.common.workset.atomic.image;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 
 import eu32k.vJoy.common.AdvancedShader;
 import eu32k.vJoy.common.Tools;
@@ -12,8 +18,22 @@ public class Animation extends ImageType {
 
    private NumberPort time = addPort(new NumberPort("Time"));
 
+   private List<String> texturePaths = new ArrayList<String>();
+   private Texture[] textures;
+
+   private AssetManager man;
+
    public Animation() {
       super("Animation");
+      man = new AssetManager();
+      File path = new File("C:/setfos_ws/vJoy/vJoy-android/assets/textures/smoke");
+      for (File file : path.listFiles()) {
+         String s = "textures/smoke/" + file.getName();
+         texturePaths.add(s);
+         man.load(s, Texture.class);
+         System.out.println("load " + s);
+      }
+      textures = new Texture[texturePaths.size()];
    }
 
    @Override
@@ -24,6 +44,15 @@ public class Animation extends ImageType {
 
          @Override
          public Texture getValue() {
+            man.update();
+            int index = MathUtils.clamp(Math.round(getPortValue(time) * texturePaths.size()), 0, texturePaths.size() - 1);
+            if (textures[index] != null) {
+               return textures[index];
+            }
+            if (man.isLoaded(texturePaths.get(index))) {
+               textures[index] = man.get(texturePaths.get(index), Texture.class);
+               return textures[index];
+            }
             return Tools.getTextrue("forest000");
          }
 
